4d85fc53eca932a222585898e56b20303ec866f6
Joel132435 <71834421+Joel132435@users.noreply.github.com>
2020-10-21 17:15:26 +0200

    Kopfspiel

--- Sprint1/Sprint1/Program.cs
+++ Sprint1/Sprint1/Program.cs
@@ -10,27 +10,89 @@ namespace Sprint1
     {
         static void Main()
         {
-            double m, z, d, h, hf, ha, p, df, c, db, deg, c1, c2 ,b;
+            double a, m, z, d, h, hf, ha, p, df, c, db, deg, x, y; //c1, c2, b,
+            int n;
+            
 
             Console.WriteLine("Bitte Geben Sie das Modul an.");
-
             m = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine();
 
             Console.WriteLine("Bitte Geben Sie die Zähnezahl an.");
-
             z = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine();
 
             Console.WriteLine("Bitte Geben Sie den Teilkreisdurchmesser an.");
-
             d = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine();
 
-            //Wird nicht gebraucht
+            //Kopfspiel
+
+            Console.WriteLine("Wollen Sie eine Kopfspiel angeben? (1 für JA und 2 für NEIN) ");
+            a = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (a < 2)
+            {
+                // x = unteres Kopfspiel / y = oberes Kopfspiel
+                x = 0.1 * m;
+                y = 0.3 * m;
+
+
+                n = 0;
+                                
+
+                // Abfrage des Kopfspiels
+
+                Console.WriteLine("Bitte geben Sie das Kopfspiel an");
+                c = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine();
+
+                // Abfrage ob das Kopfspiel zutreffen kann
+
+                while (n < 1)
+                {
+
+                    
+                    //Wenn das Kopfspiel zutrifft beendung der while-Schleife
+                    if ((c >= x) & (c <= y)) 
+                    {
+
+                        n += 1;
+
+                    }
+                    //Falls das Kopfspiel nicht zutrifft, Wert erneut abfragen
+                    else
+                    {
+
+                        Console.WriteLine("Der Angegebene Wert ist nicht korrekt.");
+                        Console.WriteLine();
+                        Console.WriteLine("Bitte geben Sie das Kopfspiel an");
+                        c = Convert.ToDouble(Console.ReadLine());
+
+                        Console.WriteLine();
+
+                        
+                    }
+
+                }
+
+            }
+            //Falls mit NEIN genatwortet wird, wird mit dem häufig verwendeten Wert gerechnet
+            else
+            {
+                c = m * 0.167;
+            }
+
+
+            //________________________________________________________________
+
+            //Folgenes wird nicht gebraucht
 
 
             //Console.WriteLine("Bitte Geben Sie die Breite an.");
@@ -38,16 +100,7 @@ namespace Sprint1
             //b = Convert.ToDouble(Console.ReadLine());
 
 
-            //Kopfspiel
-
-            //c1 = m * 0.1;
-
-            //c2 = m * 0.3;
-
-
-            //Kopfspiel
-
-            c = m * 0.167;
+            //________________________________________________________________
 
 
             //Zahnhöhe
