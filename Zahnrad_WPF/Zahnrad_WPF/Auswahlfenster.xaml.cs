65f7dab5c3bd417374a70becd424159ba269b130
Joel132435 <71834421+Joel132435@users.noreply.github.com>
2020-11-17 17:20:42 +0100

    Auswahlfenster

--- Zahnrad_WPF/Zahnrad_WPF/Auswahlfenster.xaml.cs
+++ Zahnrad_WPF/Zahnrad_WPF/Auswahlfenster.xaml.cs
@@ -19,18 +19,172 @@ namespace Zahnrad_WPF
     /// </summary>
     public partial class Auswahlfenster : Window
     {
-       
-
-        private void Label_AccessKeyPressed(object sender, AccessKeyPressedEventArgs e)
-        {
-
-        }
-
+              
         public Auswahlfenster()
         {
            
             
             InitializeComponent();
+        }
+
+        internal void trv_GeradverzahnungStirnraeder_Selected(object sender, RoutedEventArgs e)
+        {
+            grd_Aufforderung.Visibility = Visibility.Hidden;
+            grd_Ergebnisse.Visibility = Visibility.Visible;
+            grd_Geradverzahnung.Visibility = Visibility.Visible;
+
+            txt_GeVeModul.Clear();
+            txt_GeVeBreite.Clear();
+            txt_GeVeZaehnezahl.Clear();
+            txt_GeVeEingriffswinkel.Clear();
+            txt_GeVeTeilkreisdurchmesser.Clear();
+            txt_GeVeKopfspiel.Clear();
+            txt_GeVeProfilverschiebung.Clear();
+
+        }
+
+        internal void trv_PfeilverzahnungStirnraeder_Selected(object sender, RoutedEventArgs e)
+        {
+            grd_Aufforderung.Visibility = Visibility.Hidden;
+            grd_Ergebnisse.Visibility = Visibility.Visible;
+            grd_Geradverzahnung.Visibility = Visibility.Hidden;
+
+        }
+
+        internal void trv_SchraegverzahnungStirnraeder_Selected(object sender, RoutedEventArgs e)
+        {
+            grd_Aufforderung.Visibility = Visibility.Hidden;
+            grd_Ergebnisse.Visibility = Visibility.Visible;
+            grd_Geradverzahnung.Visibility = Visibility.Hidden;
+
+        }
+
+        internal void trv_DoppelschraegverzahnungStirnraeder_Selected(object sender, RoutedEventArgs e)
+        {
+            grd_Aufforderung.Visibility = Visibility.Hidden;
+            grd_Ergebnisse.Visibility = Visibility.Visible;
+            grd_Geradverzahnung.Visibility = Visibility.Hidden;
+        }
+
+        internal void trv_GeradverzahnungKegelraeder_Selected(object sender, RoutedEventArgs e)
+        {
+            grd_Aufforderung.Visibility = Visibility.Hidden;
+            grd_Ergebnisse.Visibility = Visibility.Visible;
+            grd_Geradverzahnung.Visibility = Visibility.Visible;
+
+            txt_GeVeModul.Clear();
+            txt_GeVeBreite.Clear();
+            txt_GeVeZaehnezahl.Clear();
+            txt_GeVeEingriffswinkel.Clear();
+            txt_GeVeTeilkreisdurchmesser.Clear();
+            txt_GeVeKopfspiel.Clear();
+            txt_GeVeProfilverschiebung.Clear();
+        }
+
+        internal void trv_PfeilverzahnungKegelraeder_Selected(object sender, RoutedEventArgs e)
+        {
+            grd_Aufforderung.Visibility = Visibility.Hidden;
+            grd_Ergebnisse.Visibility = Visibility.Visible;
+            grd_Geradverzahnung.Visibility = Visibility.Hidden;
+
+        }
+
+        internal void trv_GeradverzahnungSchnecke_Selected(object sender, RoutedEventArgs e)
+        {
+            grd_Aufforderung.Visibility = Visibility.Hidden;
+            grd_Ergebnisse.Visibility = Visibility.Visible;
+            grd_Geradverzahnung.Visibility = Visibility.Visible;
+
+            txt_GeVeModul.Clear();
+            txt_GeVeBreite.Clear();
+            txt_GeVeZaehnezahl.Clear();
+            txt_GeVeEingriffswinkel.Clear();
+            txt_GeVeTeilkreisdurchmesser.Clear();
+            txt_GeVeKopfspiel.Clear();
+            txt_GeVeProfilverschiebung.Clear();
+        }
+
+        internal void trv_BogenverzahnungSchnecke_Selected(object sender, RoutedEventArgs e)
+        {
+            grd_Aufforderung.Visibility = Visibility.Hidden;
+            grd_Ergebnisse.Visibility = Visibility.Visible;
+            grd_Geradverzahnung.Visibility = Visibility.Hidden;
+
+        }
+
+
+
+
+
+        internal void trv_Stirnraeder_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        internal void trv_Kegelraeder_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        internal void trv_Schnecke_Selected(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+       
+      
+        private void btn_Schließen_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (MessageBox.Show("Soll das Programm beendet werden?", "Programmende", MessageBoxButton.YesNo, MessageBoxImage.Question, MessageBoxResult.No) == MessageBoxResult.Yes)
+                System.Environment.Exit(0);
+        }
+
+        private void btn_GeVe_berechnung_Click(object sender, RoutedEventArgs e)
+        {
+
+            GeradverzahnungBerechnung geradZahn1 = new GeradverzahnungBerechnung();
+
+
+
+
+
+            string ModulEingabe, ZähnezahlEingabe, TeilkreisdurchmesserEingabe, BreiteEingabe, EIngriffswinkelEingabe, KopfspielEingabe;
+
+            ModulEingabe = txt_GeVeModul.Text;
+            ZähnezahlEingabe = txt_GeVeZaehnezahl.Text;
+            TeilkreisdurchmesserEingabe = txt_GeVeTeilkreisdurchmesser.Text;
+            BreiteEingabe = txt_GeVeBreite.Text;
+            EIngriffswinkelEingabe = txt_GeVeEingriffswinkel.Text;
+            KopfspielEingabe = txt_GeVeKopfspiel.Text;
+
+            geradZahn1.TestderModulEingabe(ModulEingabe);
+            geradZahn1.TestderZähnezahlEingabe(ZähnezahlEingabe);
+            geradZahn1.TestdesTeilkreisdurchmessersEingabe(TeilkreisdurchmesserEingabe);
+            geradZahn1.TestderBreiteEingabe(BreiteEingabe);
+            geradZahn1.TestderEingriffswinkelEingabe(EIngriffswinkelEingabe);
+            geradZahn1.TestderKopfspielEingabe(KopfspielEingabe);
+
+            geradZahn1.KontrolleWertebereichEingriffswinkel();
+            geradZahn1.KontrolleWertebereichKopfspiel();
+
+
+            lbl_Zahnhöhe.Content = geradZahn1.ZahnhöheBerechen();
+            lbl_Zahnfußhöhe.Content = geradZahn1.ZahnfußhöheBerechnen();
+            lbl_Zahnkopfhöhe.Content = geradZahn1.ZahnkopfhöheBerechnen();
+            lbl_Teilung.Content = geradZahn1.TeilungBerechnen();
+            lbl_Fußkreisdurchmesser.Content = geradZahn1.FußkreisdurchmesserBerechnen();
+            lbl_Grundkreisdurchmesser.Content = geradZahn1.GrundkreisdurchmesserBerechnen();
+            lbl_Kopfkreisdurchmesser.Content = geradZahn1.KopfkreisdurchmesserBerechnen();
+            lbl_Zahnfußfestigkeit.Content = geradZahn1.ZahnfußfestigkeitBerechnen();
+            lbl_Zahnflankenfestigkeit.Content = geradZahn1.ZahnflankenfestigkeitBerechnen();
+            lbl_Gewicht.Content = geradZahn1.GewichtBerechnen();
+            lbl_Preis.Content = geradZahn1.PreisBerechnen();
+
+
+
+
+
         }
     }
 }
---
+++ Zahnrad_WPF/Zahnrad_WPF/GeradverzahnungBerechnung.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Zahnrad_WPF
+{
+    class GeradverzahnungBerechnung
+    {
+        //Eigenschaften
+        public double Zähnezahl { get; set; }
+        public double Teilkreisdurchmesser { get; set; }
+        public double Breite { get; set; }
+        public double Modul { get; set; }
+        public double Kopfspiel { get; set; }
+        public double Eingriffswinkel { get; set; }
+
+
+
+        //Methoden zur kontrolle der eingaben auf zahlen
+        internal void TestderModulEingabe(string ModulEingabe)
+        {
+            double KontrolliertesModul;
+            int n;
+            n = 0;
+            do
+            {
+
+                if (double.TryParse(ModulEingabe, out KontrolliertesModul))
+                {
+                    n++;
+                    Modul = KontrolliertesModul;
+                }
+                else
+                {
+                    MessageBox.Show("Falsche modul eingabe bitte natürliche Zahlen eingeben");
+                    return;
+                }
+            } while (n < 1);
+        }
+
+        internal void TestderZähnezahlEingabe(string ZähnezahlEingabe)
+        {
+            double KontrollierteZähnezahl;
+            int n;
+            n = 0;
+            do
+            {
+
+                if (double.TryParse(ZähnezahlEingabe, out KontrollierteZähnezahl))
+                {
+                    n++;
+                    Zähnezahl = KontrollierteZähnezahl;
+                }
+                else
+                {
+                    MessageBox.Show("Falsche Zähnezahl eingabe bitte natürliche Zahlen eingeben");
+                    return;
+                }
+            } while (n < 1);
+        }
+
+        internal void TestdesTeilkreisdurchmessersEingabe(string TeilkreisdurchmesserEingabe)
+        {
+            double KontrollierterTeilkreisdurchmesser;
+            int n;
+            n = 0;
+            do
+            {
+
+                if (double.TryParse(TeilkreisdurchmesserEingabe, out KontrollierterTeilkreisdurchmesser))
+                {
+                    n++;
+                    Teilkreisdurchmesser = KontrollierterTeilkreisdurchmesser;
+                }
+                else
+                {
+                    MessageBox.Show("Falsche Teilkreisdurchmessereingabe eingabe bitte natürliche Zahlen eingeben");
+                    return;
+                }
+            } while (n < 1);
+        }
+
+        internal void TestderBreiteEingabe(string BreiteEingabe)
+        {
+            double KontrollierteBreite;
+            int n;
+            n = 0;
+            do
+            {
+
+                if (double.TryParse(BreiteEingabe, out KontrollierteBreite))
+                {
+                    n++;
+                    Breite = KontrollierteBreite;
+                }
+                else
+                {
+                    MessageBox.Show("Falsche Breite eingabe bitte natürliche Zahlen eingeben");
+                    return;
+                }
+            } while (n < 1);
+        }
+
+        internal void TestderEingriffswinkelEingabe(string EingriffswinkelEingabe)
+        {
+            double KontrollierterEigriffswinkel;
+            int n;
+            n = 0;
+            do
+            {
+
+                if (double.TryParse(EingriffswinkelEingabe, out KontrollierterEigriffswinkel))
+                {
+                    Eingriffswinkel = KontrollierterEigriffswinkel;
+                    n++;
+                }
+                else
+                {
+                    MessageBox.Show("Ihr Eingriffswinkel wurde auf 20 gesetzt da falsche Eingabe");
+                    Eingriffswinkel = 20;
+                    return;
+
+                }
+            } while (n < 1);
+        }
+
+        internal void TestderKopfspielEingabe(string KopfspielEingabe)
+        {
+            double KontrolliertesKopfspiel;
+            int n;
+            n = 0;
+            do
+            {
+
+                if (double.TryParse(KopfspielEingabe, out KontrolliertesKopfspiel))
+                {
+                    n++;
+                    Kopfspiel = KontrolliertesKopfspiel;
+                }
+                else
+                {
+                    MessageBox.Show("Ihr Kopfspiel wurde auf 0,167 gesetzt da falsche Eingabe");
+                    Kopfspiel = 0.167;
+                    return;
+                }
+            } while (n < 1);
+        }
+
+
+        internal void KontrolleWertebereichEingriffswinkel()
+        {
+
+            int n;
+            n = 0;
+            do
+            {
+
+                if (Eingriffswinkel <= 90 && Eingriffswinkel > 0)
+                {
+                    n++;
+
+                }
+                else
+                {
+
+                    MessageBox.Show("Bitte geben sie Zahlen im Wertebereich an (0 - 90) !", "Eingriffswinkel", MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            } while (n < 1);
+        }
+
+        internal void KontrolleWertebereichKopfspiel()
+        {
+            int n;
+            n = 0;
+            do
+            {
+
+                if (Kopfspiel <= 0.3 * Modul && Kopfspiel >= 0.1 * Modul)
+                {
+                    n++;
+
+                }
+                else
+                {
+                    MessageBox.Show("Bitte geben sie Zahlen im Wertebereich an (" + 0.1 * Modul + " - " + 0.3 * Modul + ") !", "Kopfspiel" , MessageBoxButton.OK, MessageBoxImage.Error);
+                    return;
+                }
+            } while (n < 1);
+        }
+
+        //Methoden zur berechnung
+
+
+
+
+        internal double ZahnhöheBerechen()
+        {
+            double ZahnhöheAusgabe;
+
+            ZahnhöheAusgabe = 2 * Modul + Kopfspiel;
+
+            return ZahnhöheAusgabe;
+        }
+
+        internal double ZahnfußhöheBerechnen()
+        {
+            double ZahnfußhöheAusgabe;
+
+            ZahnfußhöheAusgabe = Modul + Kopfspiel;
+
+            return ZahnfußhöheAusgabe;
+        }
+
+        internal double ZahnkopfhöheBerechnen()
+        {
+            double ZahnkopfhöheAusgabe;
+
+            ZahnkopfhöheAusgabe = Modul;
+
+            return ZahnkopfhöheAusgabe;
+        }
+
+        internal double TeilungBerechnen()
+        {
+            double TeilungAusgabe;
+
+            TeilungAusgabe = Math.PI * Modul;
+
+            return TeilungAusgabe;
+        }
+
+        internal double FußkreisdurchmesserBerechnen()
+        {
+            double FußkreisdurchmesserAusgabe;
+
+            FußkreisdurchmesserAusgabe = Teilkreisdurchmesser - (2 * (Modul + Kopfspiel));
+
+            return FußkreisdurchmesserAusgabe;
+        }
+
+        internal double GrundkreisdurchmesserBerechnen()
+        {
+            double deg, GrundkreisdurchmesserAusgabe;
+            deg = (Eingriffswinkel * (Math.PI)) / 180;
+            GrundkreisdurchmesserAusgabe = Modul * Zähnezahl * (Math.Cos(deg));
+            return GrundkreisdurchmesserAusgabe;
+        }
+
+        internal double KopfkreisdurchmesserBerechnen()
+        {
+            return 0;
+        }
+
+        internal double ZahnfußfestigkeitBerechnen()
+        {
+            return 0;
+        }
+
+        internal double ZahnflankenfestigkeitBerechnen()
+        {
+            return 0;
+        }
+
+        internal double GewichtBerechnen()
+        {
+            return 0;
+        }
+
+        internal double PreisBerechnen()
+        {
+            return 0;
+        }
+    }
+}
