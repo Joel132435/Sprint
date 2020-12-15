4e269b51d2b770f51fe2e5809fd74950cded4383
Joel132435 <71834421+Joel132435@users.noreply.github.com>
2020-12-15 21:40:15 +0100

    Update ExcelSchrägverzahnung.cs

--- Sprint3_WPF_Catia/Sprint3_WPF_Catia/ExcelSchrägverzahnung.cs
+++ Sprint3_WPF_Catia/Sprint3_WPF_Catia/ExcelSchrägverzahnung.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Sprint3_WPF_Catia
 {
